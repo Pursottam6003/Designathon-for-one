75f361bf93ca643e016ce7ddcc9a9a29c5cab836
Pursottam6003 <rahulsah6003@gmail.com>
2022-09-08 19:18:49 +0530

    updated in footer

--- src/Components/layout/Footer.js
+++ src/Components/layout/Footer.js
@@ -3,8 +3,125 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className='Footer'>
+      <div className='campusAdd'>
+        <div className='college'>
+          NIT Arunachal Pradesh
+        </div>
 
-      <p>This is footer</p>
+        <div>
+        <img src="https://img.icons8.com/color/20/000000/home--v1.png"/>
+          Jote, District: Papum Pare  Arunachal Pradesh, India - 791113
+        </div>
+
+        <div>
+        <img src="https://img.icons8.com/color/20/000000/apple-phone.png"/>
+        +91 0360-2954549
+        </div>
+
+        <div>
+        <img src="https://img.icons8.com/color/20/000000/circled-envelope.png"/>        nitapadmin@nitap.ac.in
+        </div>
+      </div>
+
+      <div className='AcademicInformations'>
+        <div className='Academic'>
+          Academic
+        </div>
+
+        <div>
+          Scholorship 
+        </div>
+
+        <div>
+          Contruction activity at Permanent Campus
+        </div>
+
+        <div>
+          Digital Initiatives by MHRD
+        </div>
+
+        <div>
+          Faculty Login
+        </div>
+
+        <div>
+          National Academic Depository (NAD)
+        </div>
+        <div>
+          NIT Moodle
+        </div>
+      </div>
+
+      
+      <div className='QuickLinks'>
+        <div className='QLinks'>
+          Quick Links
+        </div>
+
+        <div>
+          Civil Engineering Student Society
+        </div>
+
+        <div>
+            Electrical Engineering Student  Society
+        </div>
+
+        <div>
+          Electronics & Communication Engineering Student  Society
+        </div>
+
+        <div>
+          Faculty Welfare Cell
+        </div>
+
+        <div>
+          Mechanical Engineering Student Society
+        </div>
+        <div>
+          National Service Scheme(NSS)
+        </div>
+
+        <div>
+          National Cadets Corps (NCC)
+        </div>
+      </div>
+
+            
+      <div className='External Links'>
+        <div className='ExternalLinks'>
+          External Links
+        </div>
+
+        <div>
+          Academic Bank of Credits
+        </div>
+
+        <div>
+          AICTE
+        </div>
+
+        <div>
+        CCMN-2022
+        </div>
+
+        <div>
+        CCMT-2022
+        </div>
+
+        <div>
+        CCMT-CCMN-CSAB-DASA Promotions
+        </div>
+        <div>
+        CSAB-2022
+        </div>
+
+        <div>
+        JOSSA
+        </div>
+      </div>
+
+
+     
     </div>
   )
 }
