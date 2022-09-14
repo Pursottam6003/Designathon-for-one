af28dbb91b8d5bcb8270fc25aa44ac767b64c7ea
Chandrashekhar Tripathi <tripathics@outlook.com>
2022-09-14 09:03:28 +0530

    add style to list table

--- src/Components/form/List.js
+++ src/Components/form/List.js
@@ -9,10 +9,11 @@ export const List = (props) => {
       heading = "Principal investigators"
       return (
         <tr key={i}>
+          <td className="tr-number">{i+1}</td>
           <td>{item.name}</td>
           <td>{item.designation}</td>
           <td>{item.department}</td>
-          <td>
+          <td className="rm-td">
             <button className="rm" onClick={() => props.removeItem(i, "PI")}>x</button>
           </td>
         </tr>
@@ -21,11 +22,12 @@ export const List = (props) => {
       heading = "Co-principal investigators"
       return (
         <tr key={i}>
+          <td className="tr-number">{i+1}</td>
           <td>{item.name}</td>
           <td>{item.designation}</td>
           <td>{item.department}</td>
           <td>{item.insName}</td>
-          <td>
+          <td className="rm-td">
             <button className="rm" onClick={() => props.removeItem(i, "CoPI")}>x</button>
           </td>
         </tr>
@@ -34,9 +36,10 @@ export const List = (props) => {
       heading = "Authors"
       return (
         <tr key={i}>
+          <td className="tr-number">{i+1}</td>
           <td>{item.lastName}</td>
           <td>{item.firstInitials}</td>
-          <td>
+          <td className="rm-td">
             <button className="rm" onClick={() => props.removeItem(i, "author")}>x</button>
           </td>
         </tr>
