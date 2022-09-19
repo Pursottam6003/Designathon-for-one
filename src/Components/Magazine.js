30bef68fa22d93bf1ddebd89425c114e363e6d2e
Chandrashekhar Tripathi <tripathics@outlook.com>
2022-09-19 10:38:01 +0530

    add read more and toc column to magazine page

--- src/Components/Magazine.js
+++ src/Components/Magazine.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
-import jote_img from "../images/image_jote.jpg"
-import bglogo from '../images/logobgrm.png'
-import img from '../images/icons8-calendar-week-32.png'
-import {fs} from '../config/config'
+// import jote_img from "../images/image_jote.jpg"
+// import bglogo from '../images/logobgrm.png'
+// import img from '../images/icons8-calendar-week-32.png'
+import { fs } from '../config/config'
 import $ from 'jquery';
 
+const Catagories = [
+  '',
+  'Memorandum of Understanding (MoU)',
+  'Invited/Expert Lectures given by NIT AP members',
+  'Visits and Invited/Expert Lectures to NITAP from other insitutes',
+  'External Funded Projects',
+  'Patent (APA 7th edition format)',
+  'Consultancy Projects',
+  'Books',
+  'Research Papers',
+  'Conference Paper',
+  'Book Chapters',
+  'Faculty Empowerment Programmes',
+  'Reviewers',
+  'Session Chairs',
+  'Winners of Competition',
+  'Workshop/FDP/Conference/seminar/short term course etc.',
+  'Outreach Activity',
+  'Announcement',
+]
 
 const linkCopied = () => {
   var $temp = $("<input>");
@@ -23,7 +43,7 @@ const linkCopied = () => {
 
 class MagezineArticle extends Component {
   render() {
-    const {Heading, EventDate, Urls, wholeDescription, Brochure} = this.props.data
+    const { Heading, EventDate, Urls, wholeDescription, Brochure } = this.props.data
     const images = Urls.map((url, i) => {
       return (
         <img src={url} key={`i${i}`} />
@@ -44,38 +64,19 @@ class MagezineArticle extends Component {
 }
 
 class MagazineSection extends Component {
-  Catagories = [
-    '',
-    'Memorandum of Understanding (MoU)',
-    'Invited/Expert Lectures given by NIT AP members',
-    'Visits and Invited/Expert Lectures to NITAP from other insitutes',
-    'External Funded Projects',
-    'Patent (APA 7th edition format)',
-    'Consultancy Projects',
-    'Books',
-    'Research Papers',
-    'Conference Paper',
-    'Book Chapters',
-    'Faculty Empowerment Programmes',
-    'Reviewers',
-    'Session Chairs',
-    'Winners of Competition',
-    'Workshop/FDP/Conference/seminar/short term course etc.',
-    'Outreach Activity',
-    'Announcement',
-  ]
+
 
   render() {
     const { id, articles } = this.props
     const articleComponent = articles.map((article, i) => {
       return (
-        <MagezineArticle data={article} key={`a${i}`}/>
+        <MagezineArticle data={article} key={`a${i}`} />
       )
     })
     return (
       <div className='magazine-section'>
-        <header>
-          <h1>{this.Catagories[id]}</h1>
+        <header className='category-header'>
+          <h1>{Catagories[id]}</h1>
         </header>
         {articleComponent}
       </div>
@@ -86,7 +87,8 @@ class MagazineSection extends Component {
 export class Magazine extends Component {
   initialState = {
     blogs: {},
-    magSecComponents: []
+    magSecComponents: [],
+    toc: []
   }
   state = this.initialState
 
@@ -100,7 +102,7 @@ export class Magazine extends Component {
       for (var snap of blogsFirebase.docs) {
         var data = snap.data();
         data.ID = snap.id;
-        blogsarray.push({...data})
+        blogsarray.push({ ...data })
         if (blogsarray.length === blogsFirebase.docs.length) {
           this.setState({
             blogs: {
@@ -108,7 +110,7 @@ export class Magazine extends Component {
               [i]: blogsarray
             }
           }, () => {
-            if (i == 17) {
+            if (i === 17) {
               console.log('creating the blogs component')
               console.log(Object.keys(this.state.blogs));
               this.createComponents()
@@ -116,22 +118,26 @@ export class Magazine extends Component {
           })
         }
       }
-      
     }
   }
 
   createComponents() {
     console.log('Inside create components')
     const sections = []
+    const tocln = []
     Object.keys(this.state.blogs).forEach((id, key) => {
       sections.push((
         <MagazineSection id={id} key={`s${key}`} articles={this.state.blogs[id]} />
+      ))
+      tocln.push((
+        <li><a href="">{Catagories[id]}</a></li>
       ))
     })
     console.log(sections)
     console.log(this.magSecComponents)
     this.setState({
-      magSecComponents: sections
+      magSecComponents: sections,
+      toc: tocln
     })
   }
 
@@ -155,8 +161,51 @@ export class Magazine extends Component {
 
   render() {
     return (
-      <div className='magazine'>
-        {this.state.magSecComponents}
+      <div className='magazine-wrapper'>
+        <div className='magazine-toc'>
+          <div className='toc'>
+            <h4>In this article</h4>
+            {this.state.toc}
+          </div>
+        </div>
+        <div className='magazine'>
+          <ul>
+            {this.state.magSecComponents}
+          </ul>
+        </div>
+        <div className='read-more'>
+          <h5>Other releases</h5>
+          <ul>
+            <li>
+              <h6>Technodaya (Vol-V iss-3)</h6>
+              <time>Jul 23, 2022</time>
+            </li>
+            <li>
+              <h6>Technodaya (Vol-V iss-2)</h6>
+              <time>May 28, 2022</time>
+            </li>
+            <li>
+              <h6>Technodaya (Vol-V iss-1)</h6>
+              <time>Mar 24, 2022</time>
+            </li>
+            <li>
+              <h6>Technodaya (Vol-IV iss-6)</h6>
+              <time>Jan 24, 2022</time>
+            </li>
+            <li>
+              <h6>Technodaya (Vol-IV iss-5)</h6>
+              <time>Nov 17, 2021</time>
+            </li>
+            <li>
+              <h6>Technodaya (Vol-IV iss-4)</h6>
+              <time>Sep 20, 2021</time>
+            </li>
+            <li>
+              <h6>Technodaya (Vol-IV iss-3)</h6>
+              <time>Jul 13, 2021</time>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
