77f584618c53c92b24def9b7101cadbc369c80a6
daknya_bam <daknyabam18@gmil.com>
2022-10-07 17:41:56 +0530

    changed footer

--- src/Components/layout/Footer.js
+++ src/Components/layout/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import technodayaLogo from "../../images/logo/technodaya-logo-white.png"
 
 export const Footer = () => {
   return (
     <div className='Footer'>
-      <div className='campusAdd'>
+      {/* <div className='campusAdd'>
         <div className='college'>
           NIT Arunachal Pradesh
         </div>
@@ -118,10 +119,36 @@ export const Footer = () => {
         <div>
         JOSSA
         </div>
-      </div>
+      </div> */}
+
+<div className='info-Footer'>
+      <div className='rights'>
+        <div> <img id='technodayaLogo-white'  src={technodayaLogo} alt="Technodaya" />
+          
+          </div>
+        <div> <h4>Technodaya</h4>
+        <p> National Institute of Technology</p>  
+        <p> Jote , Arunchal Pradesh </p>
+        </div>
+
+        <div>
+         <p>+123456890</p> 
+         <p> <a>example@nitap.ac.in</a></p> 
 
 
-     
+
+
+        </div>
+
+    
+
+         </div>
+
+         <div className='left'>
+        content loading....
+         </div>
+         </div>
+     <div className='copyright'>&#169; 2022 all rights are preserved , Technodaya , NIT arunchal pradesh </div>
     </div>
   )
 }
