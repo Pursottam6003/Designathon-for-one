821c6b754d11a462eefbccdd05a9eedb949fe16b
Chandrashekhar Tripathi <tripathics@outlook.com>
2022-10-08 01:30:23 +0530

    add read page

--- src/Components/read.js
+++ src/Components/read.js
@@ -1,11 +1,60 @@
 import React, { Component } from "react"
+import Cover from '../images/technodaya-cover.png'
+
+class Card extends Component {
+  render() {
+    const { imgsrc, title, vol, iss, month, year, link, pdfLink } = this.props
+    return (
+      <section className="magazine-card">
+        <figure className="cover-img">
+          <a href={link} target='_blank'>
+            <img src={imgsrc} alt={`Technodaya Vol ${vol} Iss ${iss} cover`} />
+          </a>
+        </figure>
+        <div className="desc">
+          <div className="date">
+            <time>{month} {year}</time>
+            <div className="issue">Vol {vol} issue {iss}</div>
+          </div>
+          <a className="title" href={link} target='_blank'>
+            <p>{title}</p>
+          </a>
+          <div className="actions">
+            <a className="action-btn" href={pdfLink} target="_blank">View PDF</a>
+            <button className="action-btn">Share</button>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
 
 export class Read extends Component {
   render() {
     return (
       <div className="read-component route">
-        <h1>Read</h1>
+        <div className="container">
+          <header className="page-header">
+            <h1 className="heading">All releases</h1>
+          </header>
+          <div className="grid-gallery">
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+            <Card imgsrc={Cover} title={'Technodaya Vol IV, Issue 2'} vol={'IV'} iss={'2'} month={'Mar-Apr'} year={2021} link={'https://www.nitap.ac.in/news-details?slno=UE82M2lVejRPYzY4NkErTC9kYWdGdz09&notice='} pdfLink={'https://www.nitap.ac.in/storage/pdf/9112Technodaya-Vol-IV-iss-2-2021.pdf'}/>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
