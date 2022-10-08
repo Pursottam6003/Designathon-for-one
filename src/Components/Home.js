5645fc1d421911475eb305a9be23cdc59533e0ec
daknya_bam <daknyabam18@gmil.com>
2022-10-08 20:43:38 +0530

    change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,74 +1,86 @@
-import React  from 'react'
+import React from 'react'
 // import AddBlogs from '../images/AddBlogs.jpeg'
-import {ShowBlogs} from './ShowBlogs'
+import Ph from '../images/technodaya-cover.png'
+import { ShowBlogs } from './ShowBlogs'
 export const Home = () => {
   return (
     <>
-      {/* <div className='main'>
+      <div className='home-component '>
+        <div className='hero'>
 
-        <div className='blog_card'>
+          <div className='content'>
+            <h1>LATEST PUBLISHED</h1>
+            <h3> Technodaya volume 3 issu 4</h3>
+            <p>12-11-2302  july-august</p>
 
-          <img className='blogimg' src={AddBlogs} alt='this is image'></img>
-          <div className='cont'>
-            <h4>EVENT TITLE</h4>
-            <p>nit ap MOU with govt. school with jote meow meow meow meow meow meow meow meow moew meow
-            </p>
-            <button className='btn'>read article</button>
+            <a className='read-button'> <p>read</p> </a>
           </div>
+        </div>
+        <div className='route'>
+          <div className='container '>
+            <header className='page-header '>
+              <h1 className='heading'> MAGAZINE</h1>
+
+            </header>
+            <section className='issues'>
+              <div className='release-list'>
+                <div className='issues-preview'>
+                  <img className='image' src={Ph}></img>
+                  <div className='overview'>
+                    <h3> technodaya vol 1 iss 3</h3>
+                    <p> mar-april 2020  vol 1 iss 3</p>
+                    <a className='read-magazine'> read</a>
+                  </div>
+                </div>
+                <div className='issues-preview'>
+                  <img className='image' src={Ph}></img>
+                  <div className='overview'>
+                    <h3> technodaya vol 1 iss 3</h3>
+                    <p> mar-april 2020  vol 1 iss 3</p>
+                    <a className='read-magazine'> read</a>
+                  </div>
+                </div>
+                <div className='issues-preview'>
+                  <img className='image' src={Ph}></img>
+                  <div className='overview'>
+                    <h3> technodaya vol 1 iss 3</h3>
+                    <p> mar-april 2020  vol 1 iss 3</p>
+                    <a className='read-magazine'> read</a>
+                  </div>
+                </div>
+              </div>
+            </section>
+              </div>
+            </div>
+
+          <div className='publication-count'>
+              <div className='journels publication'> 123</div>
+              <div className='research-papers publication'> 123</div>
+              <div className='book chapters publication'> 123</div>
+
+            </div>
+           <div className='container'> 
+            <div className='subscription-cont'>
+           
+          <h1>subscribe to our newsletter</h1>     
+          <h3>get updated when new vol is published</h3>     
+            <form>
+            <input placeholder='first name'></input>
+            <input placeholder='last name'></input>
+              <input placeholder='email'></input>
+            
+            </form>
+
+
+              <div>
+              </div>
+          </div> 
 
         </div>
-
-
-        <div className='blog_card'>
-
-          <img className='blogimg' src={AddBlogs} alt='this is image'></img>
-          <div className='cont'>
-            <h4> EVENT TITLE</h4>
-            <p>nit ap MOU with govt. school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow
-            </p>
-            <button className='btn'>read article</button>
-          </div>
-
-        </div>
-        <div className='blog_card'>
-
-          <img className='blogimg' src={AddBlogs} alt='this is image'></img>
-          <div className='cont'>
-            <h4> EVENT TITLE</h4>
-            <p>nit ap MOU with govt. school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow
-            </p>
-            <button className='btn'>read article</button>
-          </div>
-
-        </div>
-        <div className='blog_card'>
-
-          <img className='blogimg' src={AddBlogs} alt='this is image'></img>
-          <div className='cont'>
-            <h4> EVENT TITLE</h4>
-            <p>nit ap MOU with govt. school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow
-            </p>
-            <button className='btn'>read article</button>
-          </div>
-
-        </div>
-        <div className='blog_card'>
-
-          <img className='blogimg' src={AddBlogs} alt='this is image'></img>
-          <div className='cont'>
-            <h4> EVENT TITLE</h4>
-            <p>nit ap MOU with govt. school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow school with jote meow meow meow meow meow meow meow meow moew meow
-            </p>
-            <button className='btn'>read article</button>
-          </div>
-
-        </div>
-
-      </div> */}
-      <div className='route'>
-      <ShowBlogs/>
+        
       </div>
-      
+
+
     </>
   )
 }
--- src/Components/ShowBlogs.js
+++ src/Components/ShowBlogs.js
@@ -1,5 +1,5 @@
-import React,{useState, useEffect} from 'react'
-import {fs} from '../config/config'
+import React, { useState, useEffect } from 'react'
+import { fs } from '../config/config'
 import { Items } from './Items';
 
 
@@ -15,73 +15,65 @@ const BiMonthlyNames = [
     'JulyAug',
     'SeptOct',
     'NovDec',
-  ]
+]
 
-if(month ===1 || month ===2) MonthName=BiMonthlyNames[1];
-else if(month===3 || month ===4) MonthName=BiMonthlyNames[2];
-else if(month===5 || month ===6) MonthName=BiMonthlyNames[3];
-else if(month===7 || month ===8) MonthName=BiMonthlyNames[4];
-else if(month===9 || month ===10) MonthName=BiMonthlyNames[5];
-else if(month===11 || month ===12) MonthName=BiMonthlyNames[6];
+if (month === 1 || month === 2) MonthName = BiMonthlyNames[1];
+else if (month === 3 || month === 4) MonthName = BiMonthlyNames[2];
+else if (month === 5 || month === 6) MonthName = BiMonthlyNames[3];
+else if (month === 7 || month === 8) MonthName = BiMonthlyNames[4];
+else if (month === 9 || month === 10) MonthName = BiMonthlyNames[5];
+else if (month === 11 || month === 12) MonthName = BiMonthlyNames[6];
 
 export const ShowBlogs = () => {
-    const [blogs, setblogs]=useState([]);
-    const getTechnodayaBlogs = async ()=>{
-    const blogsArray = [];
-    for(let i=1;i<=17;i++)
-    {
-        const blogs = await fs.collection(`${year}/${MonthName}/${i}`).get();
-        console.log(i,blogs.docs.length)
-       
-        // getting its snapshort 
-        for (var snap of blogs.docs){
-            var data = snap.data(); 
-            data.ID = snap.id;
-            blogsArray.push({
-                ...data
-            })
-            // console.log(myproducts)
-            if(blogsArray.length === blogs.docs.length){
-              //setting the products
-                setblogs(blogsArray);
+    const [blogs, setblogs] = useState([]);
+    const getTechnodayaBlogs = async () => {
+        const blogsArray = [];
+        for (let i = 1; i <= 17; i++) {
+            const blogs = await fs.collection(`${year}/${MonthName}/${i}`).get();
+            console.log(i, blogs.docs.length)
+
+            // getting its snapshort 
+            for (var snap of blogs.docs) {
+                var data = snap.data();
+                data.ID = snap.id;
+                blogsArray.push({
+                    ...data
+                })
+                // console.log(myproducts)
+                if (blogsArray.length === blogs.docs.length) {
+                    //setting the products
+                    setblogs(blogsArray);
+                }
             }
         }
-    }
-    console.log(blogsArray)
+        console.log(blogsArray)
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getTechnodayaBlogs();
-    },[])
-  
+    }, [])
+
     return (
-    <>
-    {blogs.length > 0 && (
-          <div className='my-products'>
-  
-              <h1 className='txt'> <span> <b>NEWS</b></span></h1>
-                  <div className='all-news'>
-                  <div className='highlight'>
-  {/* bass abi ke liya dal hu isme img , title , short ,discription  firebase se fetch karke daldena  */}
-       <div> <img className='zoom' src={require("../images/addactivity.jpg")} />  </div> <div className='writing'> 
-          <h1>THE CAT THAT SAYS MEow </h1>
-       <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi nihil vitae atque? Esse asperiores consequatur sed quo, numquam corrupti commodi officiis hic sint illo recusandae. Voluptates quasi error commodi consequuntur.
-  
-       </p> </div> 
-                  </div>
-                  <div className='news-block'>
+        <>
+            {blogs.length > 0 && (
+                <div className='my-products'>
+
                     
-                      
-                      <Items blogsArray={blogs}/>
-                  </div>
-              </div>
-          </div>
-      )}
-  
-      {blogs.length < 1 && (
-          <div className='my-products please-wait'>Please wait...</div>
-      )}
-    </>
-  )
+                    <div className='all-news'>
+                        
+                        <div className='news-block'>
+
+
+                            <Items blogsArray={blogs} />
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {blogs.length < 1 && (
+                <div className='my-products please-wait'>Please wait...</div>
+            )}
+        </>
+    )
 }
