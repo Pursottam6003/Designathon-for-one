c0db990a2c0bbadd0591b78270c76be1d10bf512
Pursottam6003 <rahulsah6003@gmail.com>
2022-10-19 03:20:39 +0530

    updates in link

--- src/Components/about.js
+++ src/Components/about.js
@@ -23,9 +23,9 @@ export class About extends Component {
                                 <p><i> Editor in chief</i></p>
                                 <p>Assistant Professor, Management & Humanities NIT AP and He is a  speaker, a listener, a demonstrator, and most of all, an influencer. He ia a teacher which is passionate, compassionate, dedicated, understanding, and supportive when it comes to their jobs and their students..</p>
                                 <ul>
-                                    <li>linkdin</li>
-                                    <li>facebook</li>
-                                    <li>instagram</li>
+                                        <li><a href="https://www.linkedin.com/in/vijayakumar-kathirvel-75ab64143/"><img src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png"/> </a></li>
+                                        <li><a href="https://www.facebook.com/kapvijayakumar"><img src="https://img.icons8.com/fluent/40/000000/facebook-new.png"/></a></li>
+                                        <li><a href="https://www.instagram.com/vijayakumarkathirvel/"><img src="https://img.icons8.com/color/40/000000/instagram-new--v1.png"/></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -40,10 +40,9 @@ export class About extends Component {
                                     <p><i> Full Stack Developer </i></p>
                                     <p> A passionate developer who is completely open to learn new technologies and help  his friends in working under tough conditions</p>
                                     <ul>
-                                        <li>linkdin</li>
-                                        <li>facebook</li>
-                                        <li>instagram</li>
-
+                                        <li><a href="https://www.linkedin.com/in/chandrashekhar-tripathi-1aa1a6201/"><img src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png"/> </a></li>
+                                        <li><a href="https://github.com/tripathics/"><img src="https://img.icons8.com/clr-gls/40/000000/github--v1.png"/></a></li>
+                                        <li><a href="https://www.instagram.com/c_strip.z/"><img src="https://img.icons8.com/color/40/000000/instagram-new--v1.png"/></a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -51,29 +50,25 @@ export class About extends Component {
 
 
                                 <div className="member-image"> <img src={puru}></img></div>
-                                <div className="member-info"><h3> Purushotam shah</h3>
+                                <div className="member-info"><h3> Pursottam Sah</h3>
                                     <p><i> Back-end,database </i></p>
                                     <p>Future CEO ,Computer Sciecne undergratuate student who is currenlty exploring all fields and also a coder who is working together in a team </p>
                                     <ul>
-                                        <li>linkdin</li>
-                                        <li>facebook</li>
-                                        <li>instagram</li>
-
+                                        <li><a href="https://www.linkedin.com/in/pursottam-sah-78aa4a1bb/"><img src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png"/> </a></li>
+                                        <li><a href="https://github.com/Pursottam6003"><img src="https://img.icons8.com/clr-gls/40/000000/github--v1.png"/></a></li>
+                                        <li><a href="https://www.instagram.com/rahulsah6003/"><img src="https://img.icons8.com/color/40/000000/instagram-new--v1.png"/></a></li>
                                     </ul>
                                 </div>
                             </div>
                             <div className="team-member-card">
-
-
                                 <div className="member-image"> <img src={Daknya}></img></div>
                                 <div className="member-info"><h3> Daknya bam</h3>
                                     <p><i> Font-end, UI-UX </i></p>
                                     <p> A coder. singer,guitarist a full pack of combination of computer science undergratuate </p>
                                     <ul>
-                                        <li>linkdin</li>
-                                        <li>facebook</li>
-                                        <li>instagram</li>
-
+                                        <li><a href=""><img src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png"/> </a></li>
+                                        <li><a href="https://github.com/daknya"><img src="https://img.icons8.com/clr-gls/40/000000/github--v1.png"/></a></li>
+                                        <li><a href="https://www.instagram.com/dak.nya/"><img src="https://img.icons8.com/color/40/000000/instagram-new--v1.png"/></a></li>
                                     </ul>
                                 </div>
                             </div>
