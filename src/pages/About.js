090f8b2da6bc4d2e94b7c9d8b1aee5e9dbed2562
Pursottam6003 <rahulsah6003@gmail.com>
2023-02-18 22:12:42 +0530

    slightly change in about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,9 @@ import Profile from "../../src/images/logo/sir.jpeg"
 import Daknya from "../../src/images/daknya.jpeg"
 import Chandrashaker from "../../src/images/tripathi.jpeg"
 import puru from "../../src/images/shah.jpeg"
-
+import ashok from '../../src/images/ashok.jpg'
+import subhajitSir from '../../src/images/subhajitSir.jpg'
+import Dev from "../../src/images/dev.jpg"
 
 export class About extends Component {
     render() {
@@ -18,24 +20,51 @@ export class About extends Component {
                             in technical education. My whole-hearted wishes and congratulations to all who involved directly and indirectly in the achievements</p></div>
                     <div className="about-team">
                         <div className="team-heading"> <h1>EDITORIAL TEAM</h1></div>
+                        <div className="Team-card-component">
                         <div className="team-member-card">
                             <div className="member-image"> <img alt="Dr. Vijayakumar" src={Profile}></img></div>
                             <div className="member-info"><h3> Dr K. Vijaykumar</h3>
                                 <p><i> Editor in chief</i></p>
-                                <p>Assistant Professor, Management & Humanities NIT AP and He is a  speaker, a listener, a demonstrator, and most of all, an influencer. He ia a teacher which is passionate, compassionate, dedicated, understanding, and supportive when it comes to their jobs and their students..</p>
+                                <p>Assistant Professor, Management & Humanities NIT AP and He is a  speaker, a listener, a demonstrator, and most of all, an influencer. He ia a teacher which is passionate, compassionate, dedicated, understanding, and supportive when it comes to their jobs and their students.</p>
                                 <ul>
                                     <li><a href="https://www.linkedin.com/in/vijayakumar-kathirvel-75ab64143/"><img alt="" src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png" /> </a></li>
                                     <li><a href="https://www.facebook.com/kapvijayakumar"><img alt="" src="https://img.icons8.com/fluent/40/000000/facebook-new.png" /></a></li>
-                                    <li><a href="https://www.instagram.com/vijayakumarkathirvel/"><img alt="" src="https://img.icons8.com/color/40/000000/instagram-new--v1.png" /></a></li>
+                                    <li><a href="#"><img alt="" src="https://img.icons8.com/color/40/000000/instagram-new--v1.png" /></a></li>
                                 </ul>
                             </div>
                         </div>
-                        <div className="team-heading"> <h1> DEVELOPERS </h1></div>
+                        <div className="team-member-card">
+                        <div className="member-image"> <img alt="Dr. Shubhajit Das" src={subhajitSir}></img></div>
+                        <div className="member-info"><h3> Dr. Shubhajit Das</h3>
+                            <p><i> Editor</i></p>
+                            <p>Assistant Professor, Department of Mechanical Engineering NIT AP.His qualities include being a great speaker and most importantly, an influencer. When it comes to his job and his students, he is passionate, compassionate, dedicated, understanding, and supportive.</p>
+                            <ul>
+                                <li><a href="https://www.linkedin.com/in/shubhajit-das-8860a873/"><img alt="" src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png" /> </a></li>
+                                <li><a href="https://www.facebook.com/06shubhajit"><img alt="" src="https://img.icons8.com/fluent/40/000000/facebook-new.png" /></a></li>
+                                <li><a href="https://www.instagram.com/vijayakumarkathirvel/"><img alt="" src="https://img.icons8.com/color/40/000000/instagram-new--v1.png" /></a></li>
+                            </ul>
+                        </div>
+                        </div>
+
+                        <div className="team-member-card">
+                        <div className="member-image"> <img alt="Mr Ashok. R" src={ashok}></img></div>
+                        <div className="member-info"><h3> Mr Ashok. R</h3>
+                            <p><i> Co-Editor</i></p>
+                            <p>Research Scholar, Department of Mechanical Engineering NIT AP. One of best student from the Department of Mechanical Engineering working very hard to achieve great sucess.Also, he is passionate about writing journels and documentations. </p>
+                            <ul>
+                                <li><a href="https://www.linkedin.com/in/vijayakumar-kathirvel-75ab64143/"><img alt="" src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png" /> </a></li>
+                                <li><a href="https://www.facebook.com/kapvijayakumar"><img alt="" src="https://img.icons8.com/fluent/40/000000/facebook-new.png" /></a></li>
+                                <li><a href="https://www.instagram.com/vijayakumarkathirvel/"><img alt="" src="https://img.icons8.com/color/40/000000/instagram-new--v1.png" /></a></li>
+                            </ul>
+                        </div>
+                        </div>
+
+                        </div>
+                        
+                        <div className="team-heading"> <h1>STUDENT EDITORS & DEVELOPERS </h1></div>
                         <div className="chutiyas">
 
                             <div className="team-member-card">
-
-
                                 <div className="member-image"> <img alt="Chandrashekhar Tripathi" src={Chandrashaker}></img></div>
                                 <div className="member-info"><h3>Chandrashaker Tripathi</h3>
                                     <p><i> Full Stack Developer </i></p>
@@ -48,8 +77,6 @@ export class About extends Component {
                                 </div>
                             </div>
                             <div className="team-member-card">
-
-
                                 <div className="member-image"> <img alt="Pursottam Sah" src={puru}></img></div>
                                 <div className="member-info"><h3>Pursottam Sah</h3>
                                     <p><i> Back-end,database </i></p>
@@ -70,6 +97,19 @@ export class About extends Component {
                                         <li><a href="https://www.linkedin.com/in/daknya-bam-4a4812237"><img alt="" src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png" /> </a></li>
                                         <li><a href="https://github.com/daknya"><img alt="" src="https://img.icons8.com/clr-gls/40/000000/github--v1.png" /></a></li>
                                         <li><a href="https://www.instagram.com/dak.nya/"><img alt="" src="https://img.icons8.com/color/40/000000/instagram-new--v1.png" /></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="team-member-card">
+                                <div className="member-image"> <img alt="Daknya Bam" src={Dev}></img></div>
+                                <div className="member-info"><h3>Dev Singh Kanyal</h3>
+                                    <p><i> Content Writer, UI-UX </i></p>
+                                    <p> An all rounder coder popularly known for performing outstanding actvities and coordination, CSE undergratuate </p>
+                                    <ul>
+                                        <li><a href="https://www.linkedin.com/in/dev-singh-kanyal/"><img alt="" src="https://img.icons8.com/fluent/40/000000/linkedin-circled.png" /> </a></li>
+                                        <li><a href="https://github.com/dev-singh-kanyal"><img alt="" src="https://img.icons8.com/clr-gls/40/000000/github--v1.png" /></a></li>
+                                        <li><a href="https://www.instagram.com/dev_singh_kanyal/"><img alt="" src="https://img.icons8.com/color/40/000000/instagram-new--v1.png" /></a></li>
                                     </ul>
                                 </div>
                             </div>
