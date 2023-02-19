960b1a42244cef4a044acd3bd63f87f8b96e6bee
Chandrashekhar Tripathi <tripathics@outlook.com>
2023-02-20 01:05:39 +0530

    fix login styles with css modules

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,8 @@ import { ReactComponent as SpinnerIcon } from '../images/icons/spinner.svg'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { getDoc, doc } from 'firebase/firestore'
+import cx from 'classnames';
+import styles from './styles/Login.module.scss';
 
 export const Login = ({ user, loginUser, logoutUser }) => {
   const history = useNavigate();
@@ -62,33 +64,33 @@ export const Login = ({ user, loginUser, logoutUser }) => {
   })
 
   return (
-    <div className='login-page route'>
+    <div className={styles['login-page']}>
       <div className='container'>
         <header className='page-header'>
           <h1 className='heading'>Login</h1>
         </header>
-        <div className='form-box'>
-          <div className='messages'>
-            {errorMsg && <div className='login-msg error'>
+        <div className={styles['form-box']}>
+          <div className={styles.messages}>
+            {errorMsg && <div className={styles['login-msg error']}>
               <div className='icon'><ErrorIcon /></div>
               {errorMsg}
             </div>}
           </div>
-          <form className='login-form' onSubmit={handleLogin}>
-            <div className='login-field email'>
-              <img alt="" src={envelop} className='icon' />
+          <form className={styles['login-form']} onSubmit={handleLogin}>
+            <div className={cx(styles['login-field'], styles.email)}>
+              <img alt="" src={envelop} className={styles.icon} />
               <input type='email' required placeholder='Email' onChange={(e) => setEmail(e.target.value)} value={email} ></input>
             </div>
-            <div className='login-field password'>
-              <img alt="" src={lock} className='icon' />
+            <div className={cx(styles['login-field'], styles.password)}>
+              <img alt="" src={lock} className={styles.icon} />
               <input type='password' onChange={(e) => setPassword(e.target.value)} value={password} required placeholder='Password'></input>
             </div>
             {loading ? (
-              <button className='login-btn' style={{ display: 'flex', justifyContent: 'center' }} disabled type="submit">
+              <button className={styles['login-btn']} style={{ display: 'flex', justifyContent: 'center' }} disabled type="submit">
                 <SpinnerIcon style={{ height: '1.2rem' }} />
               </button>
             ) : (
-              <button className='login-btn' type="submit">Login</button>
+              <button className={styles['login-btn']} type="submit">Login</button>
             )}
 
           </form>
